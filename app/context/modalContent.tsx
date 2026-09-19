04371e2a268a872ccab1bf5ae6d7aec913ab52a4
kurataniasami <kr-scream.hurryx@ezweb.ne.jp>
2026-09-19 20:04:52 +0900

    0919

--- app/context/modalContent.tsx
+++ app/context/modalContent.tsx
@@ -1,5 +1,26 @@
-import { createContext } from "react";
+'use client'
 
-const ModalContext = createContext(undefined)  // Context作成
+import { createContext, Dispatch, ReactNode, SetStateAction, useState } from "react";
 
-export const ModalProvider = ({ clildren } : { children: ReactNode})
+type ModalContextType = {
+  isOpen: boolean
+  setIsOpen: Dispatch<SetStateAction<boolean>>
+}
+
+const ModalContext = createContext<ModalContextType | undefined>(undefined)  // Context作成
+
+// モーダルの開閉の状態をContextでわたす
+export const ModalProvider = ({ children } : { children: ReactNode }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const openModal = () => {
+    setIsOpen(true)
+  }
+
+  return (
+    <ModalContext.Provider value={{isOpen, setIsOpen}}>
+      { children }
+    </ModalContext.Provider>
+  )
+}
